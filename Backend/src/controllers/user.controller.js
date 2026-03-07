e0fe5220377e20856378ce0f664c1b2c0f0d061c
amitkraj10 <amitkraj20@gmail.com>
2026-03-07 18:39:50 +0530

    Update user.controller.js

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -101,7 +101,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "none"
     }
 
     return res
